97189ca83e2762a64c32442912dd9b685f85e972
Scott Street <scott@spru.sr>
2026-01-09 17:31:20 +0200

    Add UI handler, more comprehensive controls

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { renderer } from "./renderer.js";
 import { source } from "./source.js";
 import { style } from "./style/index.js";
 import type { MewMap, MewMapOptions } from "./types.js";
-import { viewBoxForSvg } from "./utils.js";
+import { ui } from "./ui.js";
 
 export const mewmap = (options: MewMapOptions): MewMap => {
   if (options.svg === null || options.svg.tagName !== "svg") {
@@ -25,46 +25,11 @@ export const mewmap = (options: MewMapOptions): MewMap => {
     style: style(),
     svg: options.svg as SVGSVGElement,
     renderer: renderer(),
+    ui: ui(),
   };
 
   map.renderer.init(map);
-
-  addEventListeners(map);
+  map.ui.init(map);
 
   return map;
-};
-
-const addEventListeners = (map: MewMap) => {
-  new ResizeObserver(([entry]) => {
-    if (!entry?.borderBoxSize[0]) return;
-    map.camera.resize({
-      width: entry.borderBoxSize[0].inlineSize,
-      height: entry.borderBoxSize[0].blockSize,
-    });
-    map.svg.setAttribute("viewBox", viewBoxForSvg(map.camera.viewBox));
-  }).observe(map.svg);
-
-  let pointerdown = false;
-  map.svg.addEventListener("pointerdown", () => {
-    pointerdown = true;
-  });
-  document.addEventListener("pointerup", () => {
-    pointerdown = false;
-  });
-  document.addEventListener("pointermove", (event) => {
-    if (!pointerdown) return;
-    const { x: previousX, y: previousY } = map.camera.screenToTile({
-      x: event.offsetX - event.movementX,
-      y: event.offsetY - event.movementY,
-    });
-    const { x, y } = map.camera.screenToTile({
-      x: event.offsetX,
-      y: event.offsetY,
-    });
-    const { longitude, latitude } = map.camera.tileToCoordinates({
-      x: map.camera.x + previousX - x,
-      y: map.camera.y + previousY - y,
-    });
-    map.move({ longitude, latitude });
-  });
 };
---
+++ src/ui.ts
@@ -0,0 +1,122 @@
+import type { Camera, UI } from "./types.js";
+
+export const ui = (): UI => {
+  let lastTap: PointerEvent | null = null;
+  let doubleTap: PointerEvent | null = null;
+
+  return {
+    init({ camera, svg }: { camera: Camera; svg: SVGSVGElement }): void {
+      new ResizeObserver(([entry]) => {
+        if (!entry?.borderBoxSize[0]) return;
+        camera.resize({
+          width: entry.borderBoxSize[0].inlineSize,
+          height: entry.borderBoxSize[0].blockSize,
+        });
+        svg.setAttribute("viewBox", viewBoxForSvg(camera.viewBox));
+      }).observe(svg);
+
+      // prevent touch events
+      svg.addEventListener(
+        "touchstart",
+        (event) => {
+          event.preventDefault();
+        },
+        { passive: false },
+      );
+      svg.addEventListener(
+        "touchmove",
+        (event) => {
+          event.preventDefault();
+        },
+        { passive: false },
+      );
+
+      svg.addEventListener("pointerdown", (event) => {
+        svg.setPointerCapture(event.pointerId);
+
+        // track double tap
+        if (
+          lastTap &&
+          event.timeStamp - lastTap.timeStamp < 300 &&
+          Math.hypot(
+            event.clientX - lastTap.clientX,
+            event.clientY - lastTap.clientY,
+          ) < 30
+        ) {
+          doubleTap = event;
+          lastTap = null;
+        } else {
+          lastTap = event;
+        }
+      });
+
+      svg.addEventListener("pointerup", (event) => {
+        svg.releasePointerCapture(event.pointerId);
+
+        // double tap to zoom
+        if (
+          doubleTap &&
+          event.timeStamp - doubleTap.timeStamp < 300 &&
+          Math.hypot(
+            event.clientX - doubleTap.clientX,
+            event.clientY - doubleTap.clientY,
+          ) < 30
+        ) {
+          camera.move({ zoom: camera.zoom + 0.5 });
+        }
+
+        doubleTap = null;
+      });
+
+      svg.addEventListener("pointermove", (event) => {
+        if (event.buttons === 0) return;
+
+        // double tap + drag to zoom
+        if (doubleTap) {
+          return camera.move({ zoom: camera.zoom - event.movementY / 100 });
+        }
+
+        // drag to pan
+        if (event.buttons === 1) {
+          const { x: previousX, y: previousY } = camera.screenToTile({
+            x: event.offsetX - event.movementX,
+            y: event.offsetY - event.movementY,
+          });
+          const { x, y } = camera.screenToTile({
+            x: event.offsetX,
+            y: event.offsetY,
+          });
+          const { longitude, latitude } = camera.tileToCoordinates({
+            x: camera.x + previousX - x,
+            y: camera.y + previousY - y,
+          });
+          return camera.move({ longitude, latitude });
+        }
+      });
+
+      svg.addEventListener(
+        "wheel",
+        (event) => {
+          // ctrl/cmd + wheel to zoom
+          if (event.ctrlKey || event.metaKey) {
+            event.preventDefault();
+            camera.move({ zoom: camera.zoom - event.deltaY / 500 });
+          }
+        },
+        { passive: false },
+      );
+    },
+  };
+};
+
+const viewBoxForSvg = ({
+  x,
+  y,
+  width,
+  height,
+}: {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}) => `${x} ${y} ${width} ${height}`;
